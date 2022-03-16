a7af49fbfb815d6deca3ced8dab878d2bc9f78df
kiri0314 <t.kirihara0314@gmail.com>
2022-03-16 14:40:06 +0900

    ref: 不要なコメントを削除

--- command/src/execute.c
+++ command/src/execute.c
@@ -18,19 +18,3 @@ int	execute_cmd(t_exec_attr *ea)
 	}
 	return (0);
 }
-
-// int	execute_cmd(t_list *cmd)
-// {
-// 	t_exec_attr	*e_attr;
-
-// 	(void)cmd;
-// 	int argc = 0;
-// 	const char **argv = NULL;
-// 	init(&e_attr);
-// 	create_cmd_from_arg(argc, argv, e_attr);
-// 	if (is_self_cmd(argv[1]))
-// 		execute_self(e_attr);
-// 	else
-// 		execute_builtin(e_attr);
-// 	return (0);
-// }
