77275f0aaabc660118c196208a8de4dcf9c23d41
kiri0314 <t.kirihara0314@gmail.com>
2022-03-23 16:18:08 +0900

    ref: norm

--- command/src/execve_error.c
+++ command/src/execve_error.c
@@ -6,27 +6,29 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 23:12:32 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/03/17 23:12:36 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/03/23 16:17:35 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "command.h"
 
+/* Checking the owner's execute
+and read permissions. */
 static bool	can_exit(char *cmd_path)
 {
 	struct stat	stat_buf;
 
 	if (stat(cmd_path, &stat_buf) == -1)
 		exit(EXIT_FAILURE);
-	// 所有者の実行許可を確認している
 	if ((stat_buf.st_mode & S_IXUSR) != S_IXUSR)
 		return (false);
-	// 所有者の読み込み許可を確認している
 	if ((stat_buf.st_mode & S_IRUSR) != S_IRUSR)
 		return (false);
 	return (true);
 }
 
+/* Even if errno is ENOEXEC,
+change to EACCES if there is no execution authority */
 void	execve_error(int cp_errno, char *cmd_path)
 {
 	int	exit_status;
@@ -34,11 +36,10 @@ void	execve_error(int cp_errno, char *cmd_path)
 	if (cp_errno == ENOENT)
 		exit_status = 127;
 	else
-		exit_status = 126; // command not found以外は終了ステータスが126
-	// errnoがENOEXECでも実行権限がなければEACCESに変更する
+		exit_status = 126;
 	if (cp_errno == ENOEXEC && !can_exit(cmd_path))
 		cp_errno = EACCES;
-	else if (cp_errno == ENOEXEC) // ENOEXECは終了ステータス0
+	else if (cp_errno == ENOEXEC)
 		exit(EXIT_SUCCESS);
 	ft_put_cmd_error(cmd_path, strerror(cp_errno));
 	exit(exit_status);
