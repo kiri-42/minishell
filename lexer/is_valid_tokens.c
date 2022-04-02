6f750092e82f06a64040767c6172acfe6384f23d
astalow <endymion.0710@gmail.com>
2022-04-02 17:09:43 +0900

    fix: delimiterの無いheredocのセグフォを修正

--- lexer/is_valid_tokens.c
+++ lexer/is_valid_tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_valid_tokens.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tisoya <tisoya@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 23:16:02 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/03/17 23:16:03 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/04/02 17:05:25 by tisoya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 bool	is_valid_tokens(t_list *token_list)
 {
+	t_list	*prev;
+
+	prev = NULL;
 	while (token_list != NULL)
 	{
 		if (((t_token *)token_list->content)->type == TOKEN_ILLEGAL)
 			return (false);
+		if (prev != NULL && ((t_token *)prev->content)->type == TOKEN_HEREDOC
+			&& ((t_token *)token_list->content)->type != TOKEN_IDENT)
+			return (false);
+		prev = token_list;
 		token_list = token_list->next;
 	}
 	return (true);
